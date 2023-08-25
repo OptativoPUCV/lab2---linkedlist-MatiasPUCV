edbf12c0f61fc6dd0b70c025984aaa3eedc9616b
Replit user <>
2023-08-25 03:51:31 +0000

    new try

--- list.c
+++ list.c
@@ -110,6 +110,17 @@ void pushBack(List *list, void *data) {
 void pushCurrent(List *list, void *data) {
   Node* new = createNode(data);
 
+  if (list->current == list->head) {
+    printf("HEllo\n");
+    pushFront(list, data);
+    list->current = list->head;
+    return;
+  } else if (list->current == list->tail) {
+    pushBack(list, data);
+    list->current = list->tail;
+    return;
+  }
+
   Node* old = list->current;
 
   new->prev = old->prev;
